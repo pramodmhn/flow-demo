3beba1499eac7ad6b7ca2ab636d0b402059aee78
Pramod Maharjan <pramod.maharjan@datarati.com.au>
2019-10-01 15:25:01 +1000

    commit 14

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/connecttoMC', function(request, responsefromWeb) {
 })
 /**get data from MC**/
 app.get('/connecttoMCData', function(request, responsefromWeb) {
-     responsefromWeb.send("Tokenh");
+     responsefromWeb.send("Token"+token);
 	/*
     var weatherData = [
     {
