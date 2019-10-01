9962eabbba7f6ccd46ae3f6a6762f8e8305954b6
Pramod Maharjan <pramod.maharjan@datarati.com.au>
2019-10-01 12:10:20 +1000

    weather adds on added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const axios = require('axios');
 
 require('dotenv').config()
 
@@ -8,7 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   var env = process.env.APP_ENV;
   if (env == 'staging') {
     var envName = 'staging'
@@ -19,6 +20,49 @@ app.get('/', function(request, response) {
   }
   response.render('index.html', { env: envName});
 });
+
+*/
+
+app.get('/', function(request, response) {
+  response.send('Hello World1!')
+})
+
+app.get('/getweather', function(request, responsefromWeb) {
+  axios.get('https://api.weather.gov/alerts?active=1&state=MN')
+  .then(function (response) {
+    responsefromWeb.send(response.data.features);
+  })
+  .catch(function (error) {
+    console.log(error);
+    responsefromWeb.send(error);
+  });
+})
+
+/**MC Connect **/
+app.get('/connecttoMC', function(request, responsefromWeb) {
+	var conData = {
+    'clientId': process.env.CLIENT_ID,
+    'clientSecret': process.env.CLIENT_SECRET  
+  	}
+	axios({
+	  method:'post',
+	  url:'https://auth.exacttargetapis.com/v1/requestToken',
+	  data: conData,
+	  headers:{
+       'Content-Type': 'application/json',
+	  }
+	})
+	  .then(function(response) {
+	  	console.log(response);
+	  		responsefromWeb.send('Authorization Sent');
+	  		token = response.data.accessToken;
+	  	
+	}).catch(function (error) {
+	    console.log(error);
+	    responsefromWeb.send(error);
+	  });
+})
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app running at localhost:" + app.get('port'));
